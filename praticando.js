1e5a084d154564f051c7e61122c8aaf4759a5c3e
Leandro Bezerra <leandrodiasbezerra95@gmail.com>
2022-09-30 23:16:11 -0300

    praticando variaveis

---
+++ praticando.js
@@ -0,0 +1,24 @@
+//***DECLARATION
+var name;
+
+//ASSIGMENT or atribuição de valores
+name = "Leandro"
+
+//Tipo de dado que foi atribuido a variavel
+console.log(typeof name);
+
+//Agrupamento de declarações que
+let age, isHuman;
+// let age= 17;
+// let isHuman = true; 
+
+age = 17;
+isHuman = true;
+
+console.log(name, age, isHuman);
+
+//Concatenação
+console.log("O " + name + " tem " + age + " anos");
+
+//Template Literals
+console.log(`O ${name} tem ${age} anos: template literals`)
