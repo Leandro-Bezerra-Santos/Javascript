a3d55f7a5921405effa5b3f1dd406f59dcb39f04
Leandro <leandrodiasbezerra95@gmail.com>
2022-10-31 11:15:42 -0300

    For AND While

---
+++ Estruturas_Repeticao/repeticao.js
@@ -0,0 +1,16 @@
+// FOR
+
+for(let i = 0; i <= 5; i++){
+    console.log(i)
+}
+
+console.log("WHILE--------------");
+ 
+// While - ENQUANTO
+let h = 6;
+while(h <= 10  ){
+    console.log(h);
+    
+    h++;
+}
+
