a5daec2cbed889bb783f602037475563f864e4e9
Leandro Bezerra Santos <v601690@camara.sp>
2022-10-31 15:07:45 -0300

    Repeat

--- Estruturas_Repeticao/repeticao.js
+++ Estruturas_Repeticao/repeticao.js
@@ -14,3 +14,22 @@ while(h <= 10  ){
     h++;
 }
 
+// For...of
+let name = 'Leandro';
+let names = ['João', 'Paulo', 'Luana'];
+
+for(let name of names){ //Pegar um caracter da variavel
+    console.log(name)
+}
+
+// For...In
+let person ={
+    name: 'Roberto',
+    age: 30,
+    weight: 88.6
+}
+
+for(let property in person){
+    console.log(property)
+    console.log(person[property])
+}
