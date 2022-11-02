065e09a72b3126f05f0c585571a92c2993f63450
Leandro <leandrodiasbezerra95@gmail.com>
2022-11-02 12:43:54 -0300

    Gastos mensaiais calculater

--- exercicios/fluxo_de_caixa_familiar/caixa.js
+++ exercicios/fluxo_de_caixa_familiar/caixa.js
@@ -1,8 +1,27 @@
 const family = {
-    receitas: [991, 200, 200],
-    despensas: [300, 300, 120, 100, 100]
+    receitas: [991, 200],
+    despensas: [300, 300, 120, 100, 100,25, 50]
+}
+
+function sum(arr){
+    let total = 0;
+
+    for(let value of arr){
+        total += value;
+    }
+
+    return total;
 }
 
 function calc(){
-    
+    const calcularReceitas = sum(family.receitas);
+    const calcularDespensas = sum(family.despensas);
+
+    const total = calcularReceitas - calcularDespensas;
+    total.toFixed(2);
+    let boolean = total >= 0 ? `Seu saldo é positivo: ${total} R$` : `Seu saldo é negativo: ${total}R$`;
+
+    return console.log(boolean);
 }
+
+calc();
